a4d8b3dc909cacfefb99ad02fec346df2aa087e7
GustaGeballi <gustavo.geballi@hotmail.com>
2024-03-04 09:09:57 -0300

    finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        float saldo;
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        numero = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("Por favor, digite sua Agência: ");
+        agencia = sc.nextLine();
+
+        System.out.print("Por favor, digite o seu nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.print("Por favor, digite seu saldo: ");
+        saldo = sc.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque");
+
     }
 }
